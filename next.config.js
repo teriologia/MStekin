9274ab1ca936e531ad34e9d1e8ba1834ec6dd78a
mehmet Serdar Tekin <serdar.tekin@orginoacademy.com>
2019-09-22 23:08:48 +0300

    next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 // next.config.js
 const withLess = require('@zeit/next-less')
 module.exports = withLess({
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/MStekin' : '',
     cssModules: true
 })
-module.exports = {
-    // some configuration
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/MStekin' : '',
-    // another configuration
-}
