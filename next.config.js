e08ee3970c3950fd0ef763c103127c3bd73ae783
mehmet Serdar Tekin <serdar.tekin@orginoacademy.com>
2019-09-22 23:04:57 +0300

    add file for deploy

--- next.config.js
+++ next.config.js
@@ -3,3 +3,8 @@ const withLess = require('@zeit/next-less')
 module.exports = withLess({
     cssModules: true
 })
+module.exports = {
+    // some configuration
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/MStekin' : '',
+    // another configuration
+}
